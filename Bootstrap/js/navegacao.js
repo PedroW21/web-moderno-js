a72def585bf348382615b70763c338aae4ce9961
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-12-01 16:32:19 -0300

    Fim do modulo

--- Bootstrap/js/navegacao.js
+++ Bootstrap/js/navegacao.js
@@ -12,6 +12,8 @@
             .then(resp => resp.text())
             .then(html => {
                 destino.innerHTML = html
+                eval(html.match(/\<script\>([\s\S]*)\<\/script\>/)[1])
+                // uma posibilidade
             })
     }
 
