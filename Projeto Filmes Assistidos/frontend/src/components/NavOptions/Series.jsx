e167733c4a0e79feb2d47969b4e3d0f8810e6f1f
PedroW21 <pedrow21br@gmail.com>
2022-12-23 15:38:07 -0300

    Working, but in one line (the inputs)

--- Projeto Filmes Assistidos/frontend/src/components/NavOptions/Series.jsx
+++ Projeto Filmes Assistidos/frontend/src/components/NavOptions/Series.jsx
@@ -1,22 +1,135 @@
 import Main from "../templates/Main/Main";
+import { toWatchUrl, putOnDb, saveOnDb, removeOnDb } from "../Requests/Requests";
+import Form from "../templates/Form/Form";
 
-const headerProps = 
-{
-    icon: 'diamond',
-    title: 'Séries',
-    subtitle: 'CRUD de séries com separação das tabelas de séries vistos e séries para ver'
-}
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const headerProps = {
+  icon: "diamond",
+  title: "Séries",
+  subtitle:
+    "CRUD de séries com separação das tabelas de séries vistos e séries para ver",
+};
 
-const Filmes = props =>
-{
+const initialState = {
+    animeToAdd: {
+      name: "",
+      type: "series",
+      streaming: "",
+      seasons: "",
+      episodes: {
+        season1: "",
+      },
+      finished: false,
+    },
+    list: [],
+  };
 
+  const fields = {
+    name: ["name", "streaming", "seasons", "episodes", "finished"],
+    label: ["Nome", "Streaming", "Temporadas", "Episodios", "Terminou?"]
+  }
 
-    return (
-        <Main {...headerProps}>
-            deu certo meu bom 2
-        </Main>
-    )
-}
+  const formConfig = {
+    qtdFieldPerRow: 2,
+    qtdRows: 3
+  }
+
+const Filmes = (props) => {
+  const [anime, setAnime] = useState({ ...initialState });
+  const [options, setOptions] = useState([]);
+  const [numSeason, setNumSeason] = useState(1);
+
+  //load table when ready
+  useEffect(() => {
+    axios.get(toWatchUrl).then((res) => {
+      setAnime({ animeToAdd: { ...initialState.animeToAdd }, list: res.data });
+    });
+  }, []);
+
+  //render field "season" to insert eps per season
+  useEffect(() => {
+    const numberOfSeasons = parseInt(anime.animeToAdd.seasons);
+
+    let n = 1;
+    const options = [];
+
+    while (n <= numberOfSeasons) {
+      options.push(n);
+      n++;
+    }
+
+    setOptions(options);
+
+    // update qtd eps seasons
+  }, [anime.animeToAdd.seasons]);
+
+  const updateFields = (event) => {
+    let field = { ...anime.animeToAdd };
+    console.log(anime.animeToAdd);
+
+    if (event.target.name === `season${numSeason}`) {
+      field.episodes[event.target.name] = event.target.value;
+    } else {
+      field[event.target.name] = event.target.value;
+    }
+
+    setAnime({ animeToAdd: field, list: anime.list });
+  };
+
+  const editingSeason = (event) => {
+    setNumSeason(event.target.value);
+
+    const field = { ...anime.animeToAdd };
+    field.episodes[`season${event.target.value}`] =
+      anime.animeToAdd.episodes[`season${event.target.value}`] || "";
+    console.log(field.episodes);
+    setAnime({ animeToAdd: field, list: anime.list });
+  };
+
+  const save = () => {
+    if (anime.animeToAdd.id) {
+      const animeToSave = { ...anime.animeToAdd };
+      putOnDb(animeToSave);
+
+      const list = anime.list.filter((anime) => anime.id !== animeToSave.id);
+      list.push(animeToSave);
+
+      setAnime({ animeToAdd: { ...initialState.animeToAdd }, list });
+    } else {
+      saveOnDb(anime.animeToAdd);
+
+      const list = anime.list;
+      list.push(anime.animeToAdd);
+
+      setAnime({ animeToAdd: { ...initialState.animeToAdd }, list });
+    }
+  };
+
+  const cancel = () => {
+    setAnime({ animeToAdd: initialState.animeToAdd, list: anime.list });
+  };
+
+  const editAnime = (animeToEdit) => {
+    setAnime({ animeToAdd: animeToEdit, list: anime.list });
+    console.log(anime.animeToAdd);
+  };
+
+  const deleteAnime = (animeToDelete) => {
+    let list = [...anime.list];
+    list = list.filter((anime) => anime.id !== animeToDelete.id);
+
+    removeOnDb(animeToDelete.id);
+
+    setAnime({ animeToAdd: initialState.animeToAdd, list });
+  };
+
+  return (
+    <Main {...headerProps}>
+        <Form fields = {fields} formConfig= {formConfig}/>
+    </Main>
+  )
+};
 
 export default Filmes;
--- Projeto Filmes Assistidos/frontend/src/components/templates/Form/Form.jsx
+++ Projeto Filmes Assistidos/frontend/src/components/templates/Form/Form.jsx
@@ -1,113 +1,58 @@
 const Form = (props) => {
+  let qtdActualField = 0;
+
+  function renderForm(fields, fieldLabel, qtdRows, qtdFieldPerRow, renderInputs) {
+    const rows = [];
+
+    for (let i = 0; i < qtdRows; i++) {
+      rows.push(
+          <div className="row" key={i}>
+            <div className="col-12 col-md-6">
+              <div className="form-group">
+                {renderInputs(fields, fieldLabel, qtdFieldPerRow)}
+              </div>
+            </div>
+          </div>
+      );
+    }
+
+    return rows.map((e) => {
+      return (e);
+    });
+  }
+
+  function renderInputs(field, fieldLabel, qtdFieldPerRow) {
+    const fields = [];
+
+    for (let i = 0; i < qtdFieldPerRow; i++) 
+    {
+      fields.push(
+        <>
+          <label>{fieldLabel[qtdActualField]}</label>
+          <input type="text" className="form-control" 
+          name={field[qtdActualField]}/>
+        </>
+      );
+
+      qtdActualField++;
+    }
+
+    console.log(fields);
+    
+    return (
+        fields.map(e => (e))
+    );
+  }
+
   return (
     <div className="form">
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <div className="form-group">
-            <label>Nome do Anime</label>
-            <input
-              type="text"
-              className="form-control"
-              name="name"
-              value={props.animeToAdd.name}
-              onChange={(e) => props.updateField(e)}
-              placeholder="Digite o nome do Anime"
-            />
-          </div>
-        </div>
-        <div className="col-12 col-md-6">
-          <div className="form-group">
-            <label>Onde assistir?</label>
-            <input
-              type="text"
-              className="form-control"
-              name="streaming"
-              value={props.animeToAdd.streaming}
-              onChange={(e) => props.updateField(e)}
-              placeholder="Digite a plataforma para assistir o anime"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <div className="form-group">
-            <label>Quantas temporadas?</label>
-            <input
-              type="text"
-              className="form-control"
-              name="seasons"
-              value={props.animeToAdd.seasons}
-              onChange={(e) => props.updateField(e)}
-              placeholder="Digite quantas temporadas teve o anime"
-            />
-          </div>
-        </div>
-        <div className="col-12 col-md-6">
-          <div className="form-group">
-            <label>Quantos filmes?</label>
-            <input
-              type="text"
-              className="form-control"
-              name="movies"
-              value={props.animeToAdd.movies}
-              onChange={(e) => props.updateField(e)}
-              placeholder="Digite a plataforma para assistir o anime"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <div className="form-group">
-            <label htmlFor="episodesPerSeason">Temporada</label>
-            <select
-              className="form-control"
-              id="episodesPerSeason"
-              onChange={(e) => props.editSeason(e)}
-            >
-              {props.options.map((elem) => (
-                <option key={elem} value={elem}>
-                  {elem}
-                </option>
-              ))}
-            </select>
-          </div>
-        </div>
-        <div className="col-12 col-md-6">
-          <div className="form-group">
-            <label>Quantos episodios?</label>
-            <input
-              type="text"
-              className="form-control"
-              name={`season${props.numSeason}`}
-              value={props.animeToAdd.episodes[`season${props.numSeason}`]}
-              onChange={(e) => props.updateFields(e)}
-              placeholder="Digite a plataforma para assistir o anime"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-6">
-          <div className="form-group">
-            <label htmlFor="finished">Terminou?</label>
-            <select
-              name="finished"
-              id="finished"
-              className="form-control"
-              value={props.animeToAdd.finished}
-              onChange={(e) => props.updateField(e)}
-            >
-              <option value="false">Não</option>
-              <option value="true">Sim</option>
-            </select>
-          </div>
-        </div>
-      </div>
+      {renderForm(
+        props.fields.name,
+        props.fields.label,
+        props.formConfig.qtdRows,
+        props.formConfig.qtdFieldPerRow,
+        renderInputs
+      )}
 
       <div className="row">
         <div className="col-12 d-flex justify-content-end mt-2">
@@ -117,7 +62,10 @@ const Form = (props) => {
           >
             Salvar
           </button>
-          <button className="btn btn-secondary m-2" onClick={e => props.cancel()}>
+          <button
+            className="btn btn-secondary m-2"
+            onClick={(e) => props.cancel()}
+          >
             Cancelar
           </button>
         </div>
