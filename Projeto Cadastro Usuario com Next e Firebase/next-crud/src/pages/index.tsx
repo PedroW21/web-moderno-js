c5a393b7494e2611c66b5da430602f3d6382a45d
PedroW21 <pedrow21br@gmail.com>
2023-01-16 19:24:25 -0300

    So brincando

--- Projeto Cadastro Usuario com Next e Firebase/next-crud/src/pages/index.tsx
+++ Projeto Cadastro Usuario com Next e Firebase/next-crud/src/pages/index.tsx
@@ -4,7 +4,8 @@ export default function Home() {
       flex h-screen justify-center items-center
       bg-gradient-to-r from-purple-500 to-blue-600
     `}>
-       <span className="text-4xl">teste</span>
+       <span className="text-4xl cursor-pointer select-none"
+       onClick={e => console.log('oi')}>teste</span>
     </div>
   )
 }
