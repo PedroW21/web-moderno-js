bc143c0df5d595a3b5566cb611e5e6221a80cdb0
PedroW21 <pedrow21br@gmail.com>
2023-01-18 13:05:15 -0300

    Versão modificada udemy

--- Projeto Cadastro Usuario com Next e Firebase/next-crud/backend/db/ColecaoCliente.ts
+++ Projeto Cadastro Usuario com Next e Firebase/next-crud/backend/db/ColecaoCliente.ts
@@ -2,44 +2,55 @@ import firebase from "../config";
 import Cliente from "../../src/core/Cliente";
 import ClienteRepositorio from "../../src/core/ClienteRepositorio";
 
-export default class ColecaoCliente implements ClienteRepositorio {
-
-    #conversor = {
-        toFirestore(cliente: Cliente) {
+export default class ColecaoCliente implements ClienteRepositorio
+{
+    // do firebase
+    #conversor = { //transformando uma classe pra um obj
+        toFireStore(cliente: Cliente)
+        {
             return {
                 nome: cliente.nome,
                 idade: cliente.idade,
             }
         },
-        fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions): Cliente {
-            const dados = snapshot.data(options)
+        fromFireStore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions): Cliente
+        {
+            //recebemos algo do firestore e ja convertemos para nossa classe
+            const dados = snapshot.data(options);
             return new Cliente(snapshot.id, dados.nome, dados.idade)
         }
     }
-    
-    async salvar(cliente: Cliente): Promise<Cliente> {
-        if(cliente?.id) {
+
+    async salvar(cliente: Cliente): Promise<Cliente>
+    {
+        if(cliente?.id)
+        {
             await this.colecao().doc(cliente.id).set(cliente)
-            return cliente
-        } else {
-            const docRef = await this.colecao().add(cliente)
-            const doc = await docRef.get()
-            return doc.data()
+            return cliente;
+        }
+        else
+        {
+            const docRef = await this.colecao().add(cliente);
+            const doc = await docRef.get();
+            return doc.data();
         }
     }
 
     async excluir(cliente: Cliente): Promise<void> {
-        return this.colecao().doc(cliente.id).delete()
+        return this.colecao().doc(cliente.id).delete;
     }
 
-    async obterTodos(): Promise<Cliente[]> {
-        const query = await this.colecao().get()
-        return query.docs.map(doc => doc.data()) ?? []
+    async obterTodos(cliente: Cliente): Promise<Cliente[]> {
+
+        const query = await this.colecao.get()
+        return query.docs.map(doc => doc.data() ?? [])
+
     }
 
-    private colecao() {
+    private colecao()
+    {
         return firebase
-            .firestore().collection('clientes')
-            .withConverter(this.#conversor)
+        .firestore().collection('clientes')
+        .withConverter(this.#conversor)
     }
 }
